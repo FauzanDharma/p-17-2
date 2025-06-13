ae05a8482ac046cb0284b57d4b178a06e4d0cd5c
FauzanDharma <fauzandharma9@gmail.com>
2025-06-13 19:32:51 +0700

    Add files via upload

---
+++ p 17 2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+
+using namespace std;
+
+void coment(char ket[30], int n) {
+    if (n % 2 == 1) 
+        strcpy(ket, "---Bilangan Ganjil---");
+    else 
+        strcpy(ket, "---Bilangan Genap---");
+}
+
+int main() {
+    char lagi, c[30];
+    int i;
+    do {
+        
+        
+        cout << "Masukkan nilai = ";
+        cin >> i;
+        coment(c, i); 
+        cout << c;
+        cout << "\n\nIngin input lagi [Y/T]: ";
+        cin >> lagi;
+    } while (lagi == 'Y' || lagi == 'y');
+    getch();
+    return 0;
+}
